a7af26e9a7eda377380d2c560c6132736f386b3f
flyash <flyash@itcao.com>
2018-12-25 22:26:36 +0800

    Disable certificate valid

--- aria2.sh
+++ aria2.sh
@@ -3,7 +3,7 @@
 cd /etc/storage/aria2
 
 # start aria2
-./aria2c --enable-rpc --rpc-listen-all > /dev/null &
+./aria2c --enable-rpc --rpc-listen-all --check-certificate=false > /dev/null &
 
 # mount webui to www
 mount --bind ./docs /www/aria2
